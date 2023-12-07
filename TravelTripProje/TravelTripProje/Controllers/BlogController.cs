81fbe0adc6e9525d522dab997be7d6fdbde858df
haticeacun <haticeacun3355@gmail.com>
2023-12-07 11:20:37 +0300

    Blm4531 Web Projesinde yapılan web uygulamasıdır.

---
+++ TravelTripProje/TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.Take(3).ToList();
+            return View(by);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+
+            //var blogbul = c.Blogs.Where(x => x.Id == id).ToList();
+            by.Deger1=c.Blogs.Where(x => x.Id==id).ToList();
+            by.Deger2=c.Yorumlars.Where(x => x.Blogid==id).ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+            return View(by);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Classes/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+        public string Aciklama { get; set; }
+        public string BlogImage { get; set; }
+        public ICollection<Yorumlar> Yorumlars { get; set; } //1e cok iliskiyi sagladı.Bir blogun birden fazla yorumu olabilir ama bir yorumun birden fazla blogu olamaz.
+    }
+}
